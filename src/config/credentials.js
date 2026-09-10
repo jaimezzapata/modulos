b746c980ca7e86a0da52e09fe2f5d611b143f2b9
jaimezzapata <zapataval2304@gmail.com>
2026-09-09 19:57:33 -0500

    Ajuste de credenciales para Vercel con process.env

--- src/config/credentials.js
+++ src/config/credentials.js
@@ -1,10 +1,10 @@
-const DB_NAME = "campus_tid"
-const DB_USER = "root"
-const DB_PASSWORD = ""
+const DB_NAME = process.env.DB_NAME || "campus_tid"
+const DB_USER = process.env.DB_USER || "root"
+const DB_PASSWORD = process.env.DB_PASSWORD || ""
 const OBJ_SEQUELIZE = {
-    port: 3306,
+    port: process.env.DB_PORT || 3306,
     dialect: "mysql",
-    host: "localhost"
+    host: process.env.DB_HOST || "localhost"
 }
 export {
     DB_NAME, DB_PASSWORD, DB_USER, OBJ_SEQUELIZE
